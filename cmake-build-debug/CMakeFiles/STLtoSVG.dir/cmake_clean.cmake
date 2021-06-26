70b65eaf4c739381d3208fd5cb8e707c1aa2a9e8
ehsanurrub <ehsanurrub86@gmail.com>
2021-06-26 22:30:37 +0200

    add data

---
+++ cmake-build-debug/CMakeFiles/STLtoSVG.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STLtoSVG.dir/main.cpp.obj"
+  "STLtoSVG.exe"
+  "STLtoSVG.exe.manifest"
+  "STLtoSVG.pdb"
+  "libSTLtoSVG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STLtoSVG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Worldssssssssssssssssssss!" << std::endl;
+    return 0;
+}
